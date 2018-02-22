bf26e988f8a7a1b6ec32ac31353a558daaca1615
Team NIM <nim.team@sky.uk>
2018-02-22 10:52:58 +0000

    Eric: Ability to set a random location for the battleship on the gameboard.

--- src/main/java/buildgame/GameBoard.java
+++ src/main/java/buildgame/GameBoard.java
@@ -1,29 +1,54 @@
 package buildgame;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class GameBoard {
 
-    private int[] locationOfBattleship;
+    private final int MAX_SIZE = 3;
+    private int[] locationOfBattleship = new int[MAX_SIZE];
 
     public void setBattleshipLocationCells(int[] shipLocation) throws BattleshipDuplicateLocationException, BattleshipNotInConsecutiveOrderException {
+        Arrays.sort(shipLocation);
 
         if (isBattleshipInSequence(shipLocation)) {
-            locationOfBattleship = shipLocation;
+            for (int index = 0; index < MAX_SIZE; index++) {
+                locationOfBattleship[index] = shipLocation[index];
+            }
         } else if (isBattleshipLocationADuplicate(shipLocation)) {
             throw new BattleshipDuplicateLocationException("Duplicate error in setting battleship location");
         } else
             throw new BattleshipNotInConsecutiveOrderException("Not in order error in setting battleship location");
     }
 
-    public int[] getBattleshipLocationCells() {
+    public int[] getBattleshipLocation() {
         return locationOfBattleship;
     }
 
-    public boolean isBattleshipInSequence(int[] locationOfBattleship) {
+    public int[] getARandomLocation() {
+        Random rnd = new Random();
 
+        int randomNumber = rnd.nextInt(7);
+
+        int[] randomShipLocation = new int[MAX_SIZE];
+
+        if (randomNumber > 5) {
+            for (int index = 0; index < MAX_SIZE; index++) {
+                randomShipLocation[MAX_SIZE - index - 1] = randomNumber;
+                randomNumber -= 1;
+            }
+        } else {
+            for (int index = 0; index < MAX_SIZE; index++) {
+                randomShipLocation[index] = randomNumber;
+                randomNumber += 1;
+            }
+        }
+
+        return randomShipLocation;
+    }
+
+    private boolean isBattleshipInSequence(int[] locationOfBattleship) {
         for (int index = 0; index < locationOfBattleship.length - 1; index++) {
-            Arrays.sort(locationOfBattleship);
             if (locationOfBattleship[index] + 1 != locationOfBattleship[index + 1]) {
                 return false;
             }
@@ -31,10 +56,8 @@ public class GameBoard {
         return true;
     }
 
-    public boolean isBattleshipLocationADuplicate(int[] locationOfBattleship) {
-
+    private boolean isBattleshipLocationADuplicate(int[] locationOfBattleship) {
         for (int index = 0; index < locationOfBattleship.length - 1; index++) {
-            Arrays.sort(locationOfBattleship);
             if (locationOfBattleship[index] == locationOfBattleship[index + 1]) {
                 return true;
             }
--- src/test/java/buildgame/GameBoardTest.java
+++ src/test/java/buildgame/GameBoardTest.java
@@ -4,6 +4,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+
 import static org.junit.Assert.*;
 
 public class GameBoardTest {
@@ -15,9 +16,8 @@ public class GameBoardTest {
 
         gameBoard.setBattleshipLocationCells(new int[]{1, 2, 3});
 
-        assertArrayEquals(new int[]{1, 2, 3}, gameBoard.getBattleshipLocationCells());
-        assertEquals(3, gameBoard.getBattleshipLocationCells().length);
-        assertTrue(gameBoard.isBattleshipInSequence(gameBoard.getBattleshipLocationCells()));
+        assertEquals(3, gameBoard.getBattleshipLocation().length);
+        assertArrayEquals(new int[]{1, 2, 3}, gameBoard.getBattleshipLocation());
     }
 
     @Test
@@ -27,13 +27,27 @@ public class GameBoardTest {
 
         gameBoard.setBattleshipLocationCells(new int[]{1, 0, 2});
 
-        assertArrayEquals(new int[]{0, 1, 2}, gameBoard.getBattleshipLocationCells());
-        assertEquals(3, gameBoard.getBattleshipLocationCells().length);
-        assertTrue(gameBoard.isBattleshipInSequence(gameBoard.getBattleshipLocationCells()));
+        assertEquals(3, gameBoard.getBattleshipLocation().length);
+        assertArrayEquals(new int[]{0, 1, 2}, gameBoard.getBattleshipLocation());
     }
+
+    @Test
+    public void gameBoardContainsABattleshipInARandomLocation() throws BattleshipDuplicateLocationException, BattleshipNotInConsecutiveOrderException {
+
+        GameBoard gameBoard = new GameBoard();
+
+        int[] randomLocation = gameBoard.getARandomLocation();
+
+        gameBoard.setBattleshipLocationCells(randomLocation);
+
+        assertEquals(3, gameBoard.getBattleshipLocation().length);
+        assertArrayEquals(randomLocation, gameBoard.getBattleshipLocation());
+    }
+
 
     @Rule
     public ExpectedException expectedExceptionNotInOrder = ExpectedException.none();
+
     @Test
     public void gameBoardFailsAsBattleshipIsNotInConsecutiveOrder() throws Exception {
         expectedExceptionNotInOrder.expect(BattleshipNotInConsecutiveOrderException.class);
@@ -46,6 +60,7 @@ public class GameBoardTest {
 
     @Rule
     public ExpectedException expectedExceptionDuplicate = ExpectedException.none();
+
     @Test
     public void gameBoardFailsAsBattleshipContainsDuplicateLocation() throws Exception {
         expectedExceptionDuplicate.expect(BattleshipDuplicateLocationException.class);
