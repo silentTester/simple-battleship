fe7563af354618f3f24cc8c804dd72b54a6ed07b
Team NIM <nim.team@sky.uk>
2018-03-14 10:42:03 +0000

    Eric: Attempting 2D arrays - checks if in sequence

--- src/test/java/buildgame/GameBoard2DTest.java
+++ src/test/java/buildgame/GameBoard2DTest.java
@@ -4,8 +4,6 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class GameBoard2DTest {
 
